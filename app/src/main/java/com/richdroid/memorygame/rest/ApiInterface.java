c0de4472b3ead9f9d9282a96fc66d114c78dbb8c
Richa Khanna <richie.khanna10@gmail.com>
2017-02-28 21:37:18 +0530

    changing api key

--- app/src/main/java/com/richdroid/memorygame/rest/ApiInterface.java
+++ app/src/main/java/com/richdroid/memorygame/rest/ApiInterface.java
@@ -16,9 +16,9 @@ import retrofit2.http.Query;
 public interface ApiInterface {
     @GET("services/rest")
     Call<FlickrGetPhotosResponse> getPhotoSet(@Query("method") String method, @Query("format") String format, @Query("api_key") String apiKey, @Query("photoset_id") String photosetId, @Query("nojsoncallback") String noJsonCallback, @Query("per_page") String perPage);
-    // https://www.flickr.com/services/rest?method=flickr.photosets.getPhotos&format=json&api_key=2bc64dda9a84a1ce1a297ee89bc87f1a&photoset_id=72157623480041879&nojsoncallback=1&per_page=9
+    // https://www.flickr.com/services/rest?method=flickr.photosets.getPhotos&format=json&api_key=a180dfbcc3cffb7c343fac401457e547&photoset_id=72157623480041879&nojsoncallback=1&per_page=9
 
     @GET("services/rest")
     Call<FlickrGetPhotoSizesResponse> getPhotoSizes(@Query("method") String method, @Query("format") String format, @Query("api_key") String apiKey, @Query("photo_id") String photoId, @Query("nojsoncallback") String noJsonCallback);
-    // https://www.flickr.com/services/rest/?method=flickr.photos.getSizes&format=json&api_key=2bc64dda9a84a1ce1a297ee89bc87f1a&photo_id=4426188531&nojsoncallback=1
+    // https://www.flickr.com/services/rest/?method=flickr.photos.getSizes&format=json&api_key=a180dfbcc3cffb7c343fac401457e547&photo_id=4426188531&nojsoncallback=1
 }
